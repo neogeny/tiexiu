1397393fd0c72ad8c013f3be2eaceec46c777077
apalala <apalala@gmail.com>
2026-03-30 20:37:23 -0400

    [ctx] clean up

--- src/contexts/ctx.rs
+++ src/contexts/ctx.rs
@@ -7,6 +7,7 @@ use crate::grammars::{CanParse, ParseResult};
 use std::fmt::Debug;
 
 pub trait Ctx: Clone + Debug {
+    // fn resolve(&self, name: &str) -> Box<dyn CanParse<Self>>;
     fn call(self, name: &str) -> ParseResult<Self>;
     fn mark(&self) -> usize;
     fn dot(&self) -> bool;
@@ -39,6 +40,13 @@ impl<'c> StrCtx<'c> {
 }
 
 impl<'c> Ctx for StrCtx<'c> {
+    // fn resolve(&self, name: &str) -> Box<dyn CanParse<Self>> {
+    //     let rule = self.rulemap
+    //         .get(name)
+    //         .expect("Rule not found");
+    //     Box::new(rule)
+    // }
+
     fn call(self, name: &str) -> ParseResult<Self> {
         let rule = self.rulemap
             .get(name)
@@ -103,77 +111,3 @@ pub struct CtxImpl<'c> {
     pub error_msg: Option<String>,
     // pub rules: Rc<HashMap<&'c str, &'c Rule<'c, CtxImpl>>>,
 }
-//
-// impl<'c, C: Ctx> Ctximpl<'c, C: Ctx> {
-//     pub fn new(cursor: &'c dyn Cursor) -> Self {
-//         Self {
-//             cursor,
-//             cut_seen: false,
-//             error_msg: None,
-//             rules: Rc::new(HashMap::new()),
-//         }
-//     }
-//
-//     pub fn resolve(&self, name: &str) -> &Rule<'_> {
-//         self.rules.get(name).unwrap()
-//     }
-//
-//     pub fn mark(&self) -> usize {
-//         self.cursor.mark()
-//     }
-//
-//     pub fn cut(&mut self) {
-//         self.cut_seen = true;
-//     }
-//
-//     pub fn dot<'a>(&self) -> ParseResult<'a> {
-//         unimplemented!()
-//     }
-//
-//     pub fn eof_check<'a>(&self) -> ParseResult<'a> {
-//         unimplemented!()
-//     }
-//
-//     pub fn next(self) -> Result<Ctximpl<'c, C: Ctx>, Ctximpl<'c, C: Ctx>> {
-//         // do it with cursor goto(+1)?
-//         Err(self)
-//     }
-//
-//     pub fn token<'p>(self, _token: &str) -> ParseResult<'p> {
-//         // if self.cursor.token(token) {
-//         //     Ok(
-//         //         (self, Cst::Token(token.into()))
-//         //     )
-//         // }
-//         // else {
-//         //     Err(self)
-//         // }
-//         unimplemented!()
-//     }
-//
-//     pub fn pattern<'p>(self, _pattern: &str) -> ParseResult<'p> {
-//         unimplemented!()
-//     }
-//
-//     pub fn search(&self, _pattern: &str) -> ParseResult<'_> {
-//         unimplemented!()
-//     }
-// }
-//     fn token(&mut self, token: &str) -> Result<String, String>;
-//     fn pattern(&mut self, pattern: &str) -> ParseResult;
-//
-//     // Rule and Dispatch
-//     fn call(&mut self, ri: &RuleInfo) -> ParseResult;
-//     fn find_rule(&self, name: &str) -> Box<dyn Fn(&mut dyn CtxImpl) -> ParseResult>;
-//
-//     // High-level combinators (taking closures to match Python's Func)
-//     fn closure(&mut self, exp: &dyn Fn(&mut dyn CtxImpl) -> ParseResult) -> ParseResult;
-//     fn positive_closure(&mut self, exp: &dyn Fn(&mut dyn CtxImpl) -> ParseResult) -> ParseResult;
-//     fn choice(&mut self, options: Vec<Box<dyn Fn(&mut dyn CtxImpl) -> ParseResult>>) -> ParseResult;
-//
-//     // AST / Result management
-//     fn define(&mut self, keys: Vec<String>, add_keys: Option<Vec<String>>);
-//     fn last_node(&self) -> ParseResult;
-//
-//     // Errors and Guards
-//     fn fail(&mut self) -> ParseResult;
