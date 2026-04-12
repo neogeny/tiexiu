bad05cd6b6d3caff568046b9e9eb8b0f2740e28e
apalala <apalala@gmail.com>
2026-04-11 21:23:41 -0400

    [tree] implemented Translator for Tree

--- src/trees/fold.rs
+++ src/trees/fold.rs
@@ -1,6 +1,7 @@
 // Copyright (c) 2026 Juancarlo Añez (apalala@gmail.com)
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+use crate::trees::KeyValue;
 use super::tree::Tree;
 
 pub trait Translator<O> {
@@ -15,15 +16,47 @@ impl<O> Translates<O> for Tree {
     fn translate_with<T: Translator<O> + ?Sized>(&self, trans: &mut T) -> O {
         match &self {
             Tree::Nil | Tree::Bottom => trans.translate(self, &[]),
+            Tree::Text(_) => trans.translate(self, &[]),
             Tree::List(nodes) => {
                 let outputs = nodes
                     .iter()
                     .map(|branch| branch.translate_with(trans))
                     .collect::<Vec<_>>();
                 trans.translate(self, outputs.as_slice())
+            },
+            Tree::Map(map) => {
+                let outputs: Vec<O> = map.entries
+                    .iter()
+                    .map(
+                        |(k, v)| 
+                            Tree::Named(
+                                KeyValue(
+                                    k.clone(), 
+                                    v.clone()
+                                ) .into()
+                            )
+                        .translate_with(trans)
+                    )
+                    .collect();
+                trans.translate(self, outputs.as_slice())
             }
-            // ...
-            _ => trans.translate(self, &[]),
+            Tree::Node {
+                meta: _,
+                tree,
+            } => {
+                let child = vec![tree.translate_with(trans)];
+                trans.translate(self,  child.as_slice())
+            },
+            Tree::NamedAsList(keyval) |
+            Tree::Named(keyval) => {
+                let child = vec![keyval.1.translate_with(trans)];
+                trans.translate(self, child.as_slice())
+            },
+            Tree::Override(tree) |        // Sets the value of the whole 
+            Tree::OverrideAsList(tree) => {
+                let child = vec![tree.translate_with(trans)];
+                trans.translate(self, child.as_slice())
+            }
         }
     }
 }
