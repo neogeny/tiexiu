6b20c51f1f31c12023480a79b998e0870cd8c8f1
apalala <apalala@gmail.com>
2026-05-23 14:04:50 -0400

    [dist] replace jemallocator with mimalloc to benefit Windows builds

--- src/lib.rs
+++ src/lib.rs
@@ -4,26 +4,17 @@
 #![allow(clippy::collapsible_if)]
 #![allow(clippy::collapsible_match)]
 
-// #[cfg(not(feature = "dhat"))]
-// #[cfg(feature = "mimalloc")]
-// use mimalloc::MiMalloc;
-// #[cfg(not(feature = "dhat"))]
-// #[global_allocator]
-// #[cfg(feature = "mimalloc")]
-// static GLOBAL: MiMalloc = MiMalloc;
-//
-// #[cfg(feature = "dhat")]
-// #[global_allocator]
-// static ALLOC: dhat::Alloc = dhat::Alloc;
-
-#[cfg(not(target_env = "msvc"))]
-#[cfg(feature = "jemalloc")]
-use tikv_jemallocator::Jemalloc;
-
-#[cfg(not(target_env = "msvc"))]
-#[cfg(feature = "jemalloc")]
+#[cfg(not(feature = "dhat"))]
+#[cfg(feature = "mimalloc")]
+use mimalloc::MiMalloc;
+#[cfg(not(feature = "dhat"))]
 #[global_allocator]
-static GLOBAL: Jemalloc = Jemalloc;
+#[cfg(feature = "mimalloc")]
+static GLOBAL: MiMalloc = MiMalloc;
+
+#[cfg(feature = "dhat")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
 
 /// Functional API (free functions) and the OO `TieXiu` wrapper.
 pub mod api;
